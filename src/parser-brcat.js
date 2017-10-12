c326c3278f37a6fd93b3d1e92ca867b488ae0183
Andi Miller <andi@andimiller.net>
2017-10-11 20:10:24 -0400

    see if I can fix the weird async error with chunkedCharacters

--- src/parser-brcat.js
+++ src/parser-brcat.js
@@ -296,7 +296,7 @@ function build_data( )
   } );
 }
 
-var createGroupedArray = function(arr, chunkSize) {
+function createGroupedArray(arr, chunkSize) {
     var groups = [], i;
     for (i = 0; i < arr.length; i += chunkSize) {
         groups.push(arr.slice(i, i + chunkSize));
@@ -320,8 +320,8 @@ async function parseKillRecord( kill )
 
   // augment with names
   var characters = _.filter([kill.victim].concat(kill.attackers), function (e) { return typeof e.character_id == "number" });
-  var splitcharacters = createGroupedArray(characters, 250); 
-  var names = _.flatten(_.map(splitcharacters, function(cs) { return gCharacterNames(_.pluck(cs, "character_id" )) }));
+  var chunkedCharacters = createGroupedArray(characters, 250); 
+  var names = _.flatten(_.map(chunkedCharacters, function(cs) { return gCharacterNames(_.pluck(cs, "character_id" )) }));
   _.each(_.zip(characters, names), function (kv) { kv[0].name = kv[1] });
 
   // Process victim
