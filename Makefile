740055ec179680b910ca09135146fd0d7b9ab0bb
Andi Miller <andi@andimiller.net>
2017-10-26 16:06:59 -0400

    update build settings

--- Makefile
+++
@@ -1,8 +0,0 @@
-build:
-	npm install --save-dev babel-cli
-	./node_modules/.bin/babel src --out-dir dst
-	sed -i -r 's/(.*)\.([0-9]+)$/echo "\1.$((\2+1))"/ge' VERSION
-	versionfile := VERSION
-	version := $(shell cat ${versionfile})
-	docker build . -tag andimiller/br:${version}
-
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "installing babel-cli"
+npm install --save-dev babel-cli
+echo "building js"
+./node_modules/.bin/babel src --out-dir dst
+echo "bumping version number"
+sed -i -r 's/(.*)\.([0-9]+)$/echo "\1.$((\2+1))"/ge' VERSION
+version=`cat VERSION` 
+echo "committing and pushing it"
+git add VERSION
+git commit -m "bump to ${version} for release"
+git push
+echo "building docker image"
+docker build . -tag andimiller/br:${version}
+echo "pushing docker image"
+docker push andimiller/br:${version}
